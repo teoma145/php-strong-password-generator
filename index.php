c334f051f11aec4000170fc6b8c97f07041d95b5
teoma145 <matteodark145@yahoo.it>
2023-11-30 16:43:38 +0100

    milestone-2

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php 
-function generatepass(){
-$symbols = '!?&%$<>^+-*/()[]{}@#_=';
-$letters ='qwertyuiopasdfghjklzxcvbnm';
-$upletters = strtoupper($letters);
-$numbers = '0123456789';
-if(isset($_GET['Passlength'])){
-    $Passlength= $_GET['Passlength'];
-    $NewPass =''; 
-    while (strlen($NewPass)< $Passlength){
-        $valoridisponibili= $symbols.$letters.$upletters.$numbers;
-        $newChar = $valoridisponibili[rand(0,strlen($valoridisponibili)-1)];
-        if (!strpos($NewPass,$newChar)){
-            $NewPass .= $newChar;
-        }
-    }
-}
- return $NewPass;
-}
+include __DIR__.'/partials/function.php';
 
 $NewPass = generatepass();
 ?>
---
+++ partials/function.php
@@ -0,0 +1,22 @@
+<?php 
+
+function generatepass(){
+    $symbols = '!?&%$<>^+-*/()[]{}@#_=';
+    $letters ='qwertyuiopasdfghjklzxcvbnm';
+    $upletters = strtoupper($letters);
+    $numbers = '0123456789';
+    if(isset($_GET['Passlength'])){
+        $Passlength= $_GET['Passlength'];
+        $NewPass =''; 
+        while (strlen($NewPass)< $Passlength){
+            $valoridisponibili= $symbols.$letters.$upletters.$numbers;
+            $newChar = $valoridisponibili[rand(0,strlen($valoridisponibili)-1)];
+            if (!strpos($NewPass,$newChar)){
+                $NewPass .= $newChar;
+            }
+        }
+    }
+     return $NewPass;
+    }
+
+?>
